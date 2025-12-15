d6148422d39e17925fad64d22401e8ed9e617242
Ben Feather <contact@benfeather.dev>
2025-12-15 21:10:01 +1300

    WIP

---
+++ packages/common/src/ui/sonner.tsx
@@ -0,0 +1,37 @@
+import { Icon } from '@iconify/react'
+import { useTheme } from 'next-themes'
+import { Toaster as Sonner, type ToasterProps } from 'sonner'
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = 'system' } = useTheme()
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps['theme']}
+      className="toaster group"
+      icons={{
+        success: <Icon icon="tabler:circle-check" className="size-4" />,
+        info: <Icon icon="tabler:info-circle" className="size-4" />,
+        warning: <Icon icon="tabler:alert-triangle" className="size-4" />,
+        error: <Icon icon="tabler:alert-octagon" className="size-4" />,
+        loading: <Icon icon="tabler:loader" className="size-4 animate-spin" />,
+      }}
+      style={
+        {
+          '--normal-bg': 'var(--popover)',
+          '--normal-text': 'var(--popover-foreground)',
+          '--normal-border': 'var(--border)',
+          '--border-radius': 'var(--radius)',
+        } as React.CSSProperties
+      }
+      toastOptions={{
+        classNames: {
+          toast: 'cn-toast',
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
